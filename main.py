d08ee6ffa2be801dd9eeb9cb404daa4a6ea5c0c8
jiwankadyan <jiwanmps@gmail.com>
2024-05-23 04:46:41 +0530

    created a function to print tic tac toe board once

---
+++ main.py
@@ -0,0 +1,8 @@
+def printBoard():
+    print(f" 0 | 1 | 2 ")
+    print(f"---|---|---")
+    print(f" 3 | 4 | 5 ")
+    print(f"---|---|---")
+    print(f" 6 | 7 | 8 ")
+if __name__ == "__main__":
+    printBoard()
